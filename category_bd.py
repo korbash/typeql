be0c922e9079a3266cb33626e634d41800e9068a
korbash <korbash.sen@gmail.com>
2025-09-11 12:58:14 +0300

    add source as typevar to chain

--- category_bd.py
+++ category_bd.py
@@ -1,38 +1,38 @@
-from typing import final, override
+from typing import final, override, Literal
 
 from core.core import Chain, DateTime, Parametr, String
 
 
 @final
-class RegInfo(Chain):
+class RegInfo[T: str](Chain[T]):
     """user uniq id"""
 
     @property
     @override
     def id(self):
-        return String.from_parent(self, Parametr("reg_info.user_id"))
+        return String[T].from_parent(self, Parametr("reg_info.user_id"))
 
     @property
     def regDate(self):
-        return DateTime.from_parent(self, Parametr("reg_info.reg_date"))
+        return DateTime[T].from_parent(self, Parametr("reg_info.reg_date"))
 
 
 @final
-class Goods(Chain):
+class Goods[T: str](Chain[T]):
     """good uniq id"""
 
     @property
     @override
     def id(self):
-        return String.from_parent(self, Parametr("goods.good_id"))
+        return String[T].from_parent(self, Parametr("goods.good_id"))
 
     @property
     def productId(self):
-        return ProductId.from_parent(self, Parametr("goods.product_id"))
+        return ProductId[T].from_parent(self, Parametr("goods.product_id"))
 
 
 @final
-class ProductId(Chain):
+class ProductId[T: str](Chain[T]):
     """another good uniq id"""
 
     base_type: bool = False
@@ -40,61 +40,62 @@ class ProductId(Chain):
     @property
     @override
     def id(self):
-        return String.from_parent(self, Parametr("goods.product_id"))
+        return String[T].from_parent(self, Parametr("goods.product_id"))
 
     @property
     def goodId(self):
-        return Goods.from_parent(self, Parametr("goods.goods_id"))
+        return Goods[T].from_parent(self, Parametr("goods.goods_id"))
 
 
 @final
-class Deals(Chain):
+class Deals[T: str](Chain[T]):
     """deal uniq id"""
 
     @property
     @override
     def id(self):
-        return String.from_parent(self, Parametr("deals.deal_id"))
+        return String[T].from_parent(self, Parametr("deals.deal_id"))
 
     @property
     def buyerId(self):
-        return RegInfo.from_parent(self, Parametr("deals.buyer_id"))
+        return RegInfo[T].from_parent(self, Parametr("deals.buyer_id"))
 
     @property
     def sellerId(self):
-        return RegInfo.from_parent(self, Parametr("deals.seller_id"))
+        return RegInfo[T].from_parent(self, Parametr("deals.seller_id"))
 
     @property
     def goodId(self):
-        return Goods.from_parent(self, Parametr("deals.good_id"))
+        return Goods[T].from_parent(self, Parametr("deals.good_id"))
 
     @property
     def dealDate(self):
-        return DateTime.from_parent(self, Parametr("deals.deal_date"))
+        return DateTime[T].from_parent(self, Parametr("deals.deal_date"))
 
 
 class BD:
     @property
     def deals(self):
-        return Deals.from_source("deals")
+        return Deals[Literal["deals"]]("deals")
 
     @property
     def reg_info(self):
-        return RegInfo.from_source("reg_info")
+        return RegInfo[Literal["reg_info"]]("reg_info")
 
     @property
     def goods(self):
-        return Goods.from_source("goods")
+        return Goods[Literal["goods"]]("goods")
 
     @property
     def productId(self):
-        return Goods.from_source("goods.product_id")
+        return Goods[Literal["goods.product_id"]]("goods.product_id")
 
 
 bd = BD()
 buyer_reg_date = bd.deals.buyerId.regDate
 seller_reg_date = bd.deals.sellerId.regDate
 product_chain = bd.deals.goodId.productId.goodId.productId
+user_reg_date = bd.reg_info.regDate
 
 # Test examples to show the new Chain pattern
 print("\n=== Chain pattern examples ===")
--- core/core.py
+++ core/core.py
@@ -2,6 +2,7 @@ from typing import final, override, Self
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
 
+
 @dataclass
 class Parametr:
     column: str
@@ -18,36 +19,32 @@ class Exp:
     arg1: Self | Parametr
 
 
-class Chain(ABC):
-
+class Chain[SourceT: str](ABC):
     def __init__(
         self,
-        source: str,
-        parent: "Chain | None" = None,
-        relation: "Parametr | Exp | None" = None
+        source: SourceT,
+        parent: "Chain[SourceT] | None" = None,
+        relation: Parametr | Exp | None = None,
     ) -> None:
         """Private constructor - use from_source() or from_parent() instead"""
-        self.source: str = source
-        self.parent: "Chain | None" = parent
-        self.relation: "Parametr | Exp | None" = relation
+        self.source: SourceT = source
+        self.parent: Chain[SourceT] | None = parent
+        self.relation: Parametr | Exp | None = relation
 
         # Определяем базовый тип
         relation_str = str(self.relation or self.source)
-        self.base_type: bool = relation_str.startswith("base_type.") or self.source.startswith("base_type.")
+        self.base_type: bool = relation_str.startswith(
+            "base_type."
+        ) or self.source.startswith("base_type.")
 
     @classmethod
-    def from_source(cls, source: str) -> Self:
-        """Create root chain element"""
-        return cls(source=source)
-
-    @classmethod
-    def from_parent(cls, parent: "Chain", relation: Parametr | Exp) -> Self:
+    def from_parent(cls, parent: "Chain[SourceT]", relation: Parametr | Exp):
         """Create child chain element"""
         return cls(source=parent.source, parent=parent, relation=relation)
 
     @property
     @abstractmethod
-    def id(self) -> "Chain":
+    def id(self) -> "Chain[SourceT]":
         """Beautiful representation of Chain with full path"""
 
     @override
@@ -68,39 +65,40 @@ class Chain(ABC):
 
 
 @final
-class DateTime(Chain):
+class DateTime[T: str](Chain[T]):
     """Any Date"""
 
     @property
     @override
     def id(self):
-        return Null.from_parent(self, Parametr("base_type.date_time"))
+        return Null[T].from_parent(self, Parametr("base_type.date_time"))
 
 
 @final
-class Number(Chain):
+class Number[T: str](Chain[T]):
     """Any Number"""
 
     @property
     @override
     def id(self):
-        return Null.from_parent(self, Parametr("base_type.number"))
+        return Null[T].from_parent(self, Parametr("base_type.number"))
 
 
 @final
-class String(Chain):
+class String[T: str](Chain[T]):
     """Any String"""
 
     @property
     @override
     def id(self):
-        return Null.from_parent(self, Parametr("base_type.string"))
+        return Null[T].from_parent(self, Parametr("base_type.string"))
+
 
 @final
-class Null(Chain):
+class Null[T: str](Chain[T]):
     """Base type for all SQL types"""
 
     @property
     @override
     def id(self):
-        return Null.from_parent(self, Parametr("base_type.null"))
+        return Null[T].from_parent(self, Parametr("base_type.null"))
